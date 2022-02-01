93f7dddd08152010b4f067c089dc9c3943842135
ValentinaKarnaukh <valia.karnaukh@yandex.ru>
2022-02-01 23:55:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isGreenLight = false;
+
+        int loser = 0;
+
+        int speedOfPlayer1 = 5;
+        int speedOfPlayer2 = 0;
+        int speedOfPlayer3 = 2;
+
+        if (speedOfPlayer1 != 0) {
+            loser = loser + 1;
+        }
+
+        if (speedOfPlayer2 != 0) {
+            loser = loser + 1;
+        }
+
+        if (speedOfPlayer3 != 0) {
+            loser = loser + 1;
+        }
+
+        System.out.println("Выбыло" + loser);
+    }
+}

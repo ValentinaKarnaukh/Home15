241c0fd3568b453b5bd6a921a1fcdec531635fcb
ValentinaKarnaukh <valia.karnaukh@yandex.ru>
2022-02-12 19:06:29 +0300

    Array

--- src/HomeWork16.java
+++ src/HomeWork16.java
@@ -1,42 +1,42 @@
 public class HomeWork16 {
-        boolean isGreenLight = false;
+    boolean isGreenLight = false;
 
-        public static boolean getSpeed1(int speedOfPlayer1) {
-            int speedWin = 0;
-            if (speedOfPlayer1 > speedWin) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        public static boolean getSpeed2(int speedOfPlayer2) {
-            int speedWin = 0;
-            if (speedOfPlayer2 > speedWin) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        public static boolean getSpeed3(int speedOfPlayer3) {
-            int speedWin = 0;
-            if (speedOfPlayer3 > speedWin) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        public static void main(String[] args) {
-            getSpeed1(5);
-            getSpeed2(0);
-            getSpeed3(2);
-
-            System.out.println(getSpeed1(5));
-            System.out.println(getSpeed2(0));
-            System.out.println(getSpeed3(2));
-
-
+    public static boolean getSpeed1(int speedOfPlayer1) {
+        int speedWin = 0;
+        if (speedOfPlayer1 > speedWin) {
+            return true;
+        } else {
+            return false;
         }
     }
+
+    public static boolean getSpeed2(int speedOfPlayer2) {
+        int speedWin = 0;
+        if (speedOfPlayer2 > speedWin) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean getSpeed3(int speedOfPlayer3) {
+        int speedWin = 0;
+        if (speedOfPlayer3 > speedWin) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        getSpeed1(5);
+        getSpeed2(0);
+        getSpeed3(2);
+
+        System.out.println(getSpeed1(5));
+        System.out.println(getSpeed2(0));
+        System.out.println(getSpeed3(2));
+
+
+    }
+}

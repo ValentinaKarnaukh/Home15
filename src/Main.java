51fa94c5955e2eefcfc85c7374ac9e69c40e8568
ValentinaKarnaukh <valia.karnaukh@yandex.ru>
2022-02-15 19:41:44 +0300

    Change all win

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,20 @@ public class Main {
 
         int loser = 0;
 
-        int speedOfPlayer1 = 1;
+        int speedOfPlayer1 = 0;
         int speedOfPlayer2 = 0;
-        int speedOfPlayer3 = 2;
+        int speedOfPlayer3 = 0;
 
-        if (speedOfPlayer1 != 0) {
-            loser = loser + 1;
-        }
-
-        if (speedOfPlayer2 != 0) {
-            loser = loser + 1;
-        }
-
-        if (speedOfPlayer3 != 0) {
-            loser = loser + 1;
+        if (isGreenLight == false) {
+            if (speedOfPlayer1 != 0) {
+                loser = loser + 1;
+            }
+            if (speedOfPlayer2 != 0) {
+                loser = loser + 1;
+            }
+            if (speedOfPlayer3 != 0) {
+                loser = loser + 1;
+            }
         }
 
         System.out.println("Выбыло" + loser);

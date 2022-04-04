78ad1df66092f521831a2c33a64909dd92b7dbff
ValentinaKarnaukh <valia.karnaukh@yandex.ru>
2022-04-04 20:39:26 +0300

    Correct code

--- src/HomeWork17.java
+++ src/HomeWork17.java
@@ -5,34 +5,45 @@ public class HomeWork17 {
 
     public static int amountLoser(int[] speeds) {
         int speedWin = 0;
-        int Loser = 0;
-        for (int i = 0; i < speeds.length; i++) {
-            if (speedWin != speeds[i]) {
-                Loser = Loser + 1;
+        int loser = 0;
+        if (isGreenLight) {
+            loser = 0;
+        } else {
+            for (int i = 0; i < speeds.length; i++) {
+                if (speedWin != speeds[i]) {
+                    loser++;
+                }
             }
         }
-
-        return Loser;
+        return loser;
     }
 
     public static int[] arraySpeedLoser(int[] speeds) {
         int speedWin = 0;
         int cnt = 0;
         for (int speed : speeds) {
-            if (speedWin != speed) {
-                cnt++;
+            if (isGreenLight) {
+                continue;
+            } else {
+                if (speedWin != speed) {
+                    cnt++;
+                }
             }
         }
 
         int[] speedsLoser = new int[cnt];
         int i = 0;
         for (int speed : speeds) {
-            if (speedWin != speed) {
-                speedsLoser[i] = speed;
-                i++;
+            if (isGreenLight) {
+                continue;
+            } else {
+                if (speedWin != speed) {
+                    speedsLoser[i] = speed;
+                    i++;
+                }
             }
-        }
 
+        }
         return speedsLoser;
     }
 
@@ -40,17 +51,25 @@ public class HomeWork17 {
         int speedWin = 0;
         int cnt = 0;
         for (int speed : speeds) {
-            if (speedWin == speed) {
-                cnt++;
+            if (isGreenLight) {
+                continue;
+            } else {
+                if (speedWin == speed) {
+                    cnt++;
+                }
             }
         }
 
         int[] speedsWin = new int[cnt];
         int i = 0;
         for (int speed : speeds) {
-            if (speedWin == speed) {
-                speedsWin[i] = speed;
-                i++;
+            if (isGreenLight) {
+                continue;
+            } else {
+                if (speedWin == speed) {
+                    speedsWin[i] = speed;
+                    i++;
+                }
             }
         }
         return speedsWin;
